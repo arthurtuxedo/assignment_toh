fc75d8adecf88e806efcb2ae30845b564b605ab4
Justin Flynn <arthurtuxedo@gmail.com>
2017-04-02 09:47:02 -0700

    Added towers.rb
    Added the towers.rb file with a high-level description of the problem.

---
+++ towers.rb
@@ -0,0 +1,10 @@
+# Towers of Hanoi
+# We have 3 rings and 3 discs of increasing size
+# We need a method of moving the top-most disc to another ring that does not
+#   already have a smaller ring on top.
+# We need to keep track of where each disc is and / or the disc configuration
+#   of each ring. The rings can have 3 possible size values on the bottom stack,
+#   2 on the middle, and only 1 on top.
+# We must make sure that the user enters an integer between 1 and 3 on the from
+#   and to inputs.
+# We must render some kind of visual representation after each move.
